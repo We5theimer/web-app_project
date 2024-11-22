10591478f07dadfb43df54279b881cb8fdee8d3b
We5theimer <132555863+We5theimer@users.noreply.github.com>
2024-11-22 21:54:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+def is_even(number):
+    return number % 2 == 0
+
 def divide(a, b):
-    """Функция деления с проверкой на деление на ноль."""
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
-
-def is_even(number):
-    """Проверяет, является ли число четным."""
-    return number % 2 == 0
