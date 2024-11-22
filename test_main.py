46a61c81c5008439c323ae4da23027540215261a
We5theimer <132555863+We5theimer@users.noreply.github.com>
2024-11-22 21:54:58 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,16 +1,15 @@
-import pytest
-from main import divide, is_even
+import unittest
+from main import is_even, divide
 
-def test_divide_by_zero():
-    """Тест на деление на ноль."""
-    with pytest.raises(ValueError):
-        divide(1, 0)
+class TestMain(unittest.TestCase):
+    def test_is_even(self):
+        self.assertTrue(is_even(4))
+        self.assertFalse(is_even(5))
 
-def test_divide():
-    """Тест на обычное деление."""
-    assert divide(6, 2) == 3
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
 
-def test_is_even():
-    """Тест на четность числа."""
-    assert is_even(4) is True
-    assert is_even(3) is False
+if __name__ == '__main__':
+    unittest.main()
